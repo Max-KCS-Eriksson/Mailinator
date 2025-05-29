4dd46be48fce36ec06d23486bd70e793569f94c9
Max Eriksson <teratoscincus@hotmail.com>
2025-05-29 16:44:35 +0200

    Init Maven project

---
+++ src/main/java/com/maxeriksson/mailinator/Main.java
@@ -0,0 +1,13 @@
+package com.maxeriksson.mailinator;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
