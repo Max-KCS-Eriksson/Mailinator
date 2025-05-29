b9f33b38726d65113eb5339d1b1bc46a4f5940f5
Max Eriksson <teratoscincus@hotmail.com>
2025-05-29 17:34:27 +0200

    Scaffold classes for mail composition

---
+++ src/main/java/com/maxeriksson/mailinator/formatter/ScanCompanyList.java
@@ -0,0 +1,11 @@
+package com.maxeriksson.mailinator.formatter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScanCompanyList {
+
+    public List<CompanyMailDetails> getCompanyMailDetailList() {
+        return new ArrayList<>(); // TODO: IMPLEMENT
+    }
+}
