6182b85bc97f7dc09f9e6584ec6b45a1d4e4650a
david rydman <david.rydman@iths.se>
2025-05-30 12:17:17 +0200

    Created BufferedReader for CSV scanning

--- src/main/java/com/maxeriksson/mailinator/Main.java
+++ src/main/java/com/maxeriksson/mailinator/Main.java
@@ -1,13 +1,16 @@
 package com.maxeriksson.mailinator;
 
+import static com.maxeriksson.mailinator.formatter.ScanCompanyList.CSVReaderDemo;
+
 /**
  * Hello world!
  *
  */
-public class Main 
+public class Main
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        CSVReaderDemo();
     }
 }
--- src/main/java/com/maxeriksson/mailinator/formatter/ScanCompanyList.java
+++ src/main/java/com/maxeriksson/mailinator/formatter/ScanCompanyList.java
@@ -1,11 +1,49 @@
 package com.maxeriksson.mailinator.formatter;
 
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ScanCompanyList {
 
     public List<CompanyMailDetails> getCompanyMailDetailList() {
         return new ArrayList<>(); // TODO: IMPLEMENT
+    }
+
+    public static void CSVReaderDemo() {
+        System.out.println("Reading CSV using BufferedReader!");
+
+        // Path to our CSV file
+        File csvFile = new File("CompanyList.csv");
+
+        // Lists to store our data
+        List<List<String>> data = new ArrayList<>();
+
+        // // Try-with-resources to ensure the reader gets closed automatically
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
+
+            // Read each line from the file
+            while ((line = br.readLine()) != null) {
+                // Split the line by comma and convert to a List
+                String[] values = line.split("\\$");
+                List<String> lineData = Arrays.asList(values);
+
+                // Add the line data to our main list
+                data.add(lineData);
+            }
+
+            // Print the data we read
+            System.out.println("\nData read from CSV file:");
+            for (int i = 0; i < data.size(); i++) {
+                List<String> row = data.get(i);
+                System.out.println("Row " + i + ": " + String.join(", ", row));
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading the CSV file: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
