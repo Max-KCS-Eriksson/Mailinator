522f3c2053876dfebfa9df535db74dfceec97f79
Max Eriksson <teratoscincus@hotmail.com>
2025-06-10 13:48:36 +0200

    Specify email recipient CSV file separator
    Specify the separator char in `email.properties`. If none is specified
    it defaults to `';'`.

--- src/main/java/com/maxeriksson/mailinator/formatter/ScanCompanyList.java
+++ src/main/java/com/maxeriksson/mailinator/formatter/ScanCompanyList.java
@@ -2,6 +2,9 @@ package com.maxeriksson.mailinator.formatter;
 
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
+
 import java.io.*;
 import java.util.List;
 
@@ -9,15 +12,19 @@ public class ScanCompanyList {
 
     private String csvFilePath;
 
+    @Value("email.recipientListSeparator")
+    private String csvSeparator;
+
     public ScanCompanyList(String csvFilePath) {
         this.csvFilePath = csvFilePath;
+        this.csvSeparator = csvSeparator == null ? ";" : csvSeparator;
     }
 
     public List<CompanyMailDetails> getCompanyMailDetailList() {
         try (FileReader reader = new FileReader(csvFilePath)) {
             CsvToBean<CompanyMailDetails> csvToBean = new CsvToBeanBuilder<CompanyMailDetails>(reader)
                     .withType(CompanyMailDetails.class)
-                    .withSeparator('$')
+                    .withSeparator(csvSeparator.toCharArray()[0])
                     .withIgnoreLeadingWhiteSpace(true)
                     .withThrowExceptions(false)
                     .build();
